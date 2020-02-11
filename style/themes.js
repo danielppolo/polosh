462b093690fc4d1b36ae72dc6b318a26e08f8cae
Daniel Polo <od.polo@gmail.com>
2020-02-10 21:48:42 -0600

    feat: Color vars

--- style/themes.js
+++ style/themes.js
@@ -10,3 +10,16 @@ export default {
     black: '#000004',
   },
 }
+
+
+// Blue
+// #12118B d
+// #26259D c
+// #3534B8 b
+// #5555D0 a
+
+// Red
+// #E00A13 b
+// #C6020A c
+// #AC0208 d
+// #FD2B34 a
