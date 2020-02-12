ed6278a939549108f6bf26aa4997e594cf739801
Daniel Polo <od.polo@gmail.com>
2020-02-12 00:41:04 -0600

    fix

--- config/firebase.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  apiKey: '***',
-  authDomain: '***',
-  databaseURL: '***',
-  projectId: '420425462870-1qagnkshdvut7qtke0pnb29cov3n86g6.apps.googleusercontent.com',
-  storageBucket: '***',
-  messagingSenderId: '***',
-}
-export default config
